a8c930e8d485dec2cfd0f06e3d02b2c304a0cdb0
nstgic3 <nstgic3@gmail.com>
2022-11-27 13:40:14 +0900

    Update categories model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
+
+class Category(CommonModel):
+
+    """Room or Experience Category"""
+
+    class CategoryKindChoices(models.TextChoices):
+        ANNOUNCES = "announces", "공지사항"
+        BENEFITS = "benefits", "제휴혜택"
+        INQUIRIES = "inquiries", "문의/건의"
+        PETITIONS = "petitions", "청원 게시판"
+        SURVEYS = "serveys", "설문조사"
+
+    name = models.CharField(
+        max_length=50,
+    )
+    kind = models.CharField(
+        max_length=15,
+        choices=CategoryKindChoices.choices,
+    )
+
+    def __str__(self) -> str:
+        return f"{self.kind.title()}: {self.name}"
+
+    class Meta:
+        verbose_name_plural = "Categories"
